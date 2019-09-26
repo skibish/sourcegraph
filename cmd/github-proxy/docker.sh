d8f861a7878a2495c0d4b8df3225294bd33cbb06
ggilmore <geoffrey@sourcegraph.com>
2019-09-26 12:27:41 -0700

    use multi-stage dockerfiles

---
+++ cmd/github-proxy/docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# We want to build multiple go binaries, so we use a custom build step on CI.
+cd $(dirname "${BASH_SOURCE[0]}")/../..
+set -ex
+
+docker build -f cmd/github-proxy/Dockerfile -t $IMAGE . \
+    --build-arg COMMIT_SHA \
+    --build-arg DATE \
+    --build-arg VERSION
