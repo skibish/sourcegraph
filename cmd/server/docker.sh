823e4ae36592b90dff23e426e925c08728ffd300
ggilmore <geoffrey@sourcegraph.com>
2019-10-02 20:22:24 -0700

    try dot again?

--- cmd/server/docker.sh
+++ cmd/server/docker.sh
@@ -23,9 +23,7 @@ if [[ "$CI" == "true" ]]; then
         fi
     done
 
-    gcloud builds submit \
-        --config=cmd/server/cloudbuild.yaml \
-        --substitutions=$substitutions
+    gcloud builds submit --config=cmd/server/cloudbuild.yaml --substitutions=$substitutions .
 else
 
     build_arg_str=""
