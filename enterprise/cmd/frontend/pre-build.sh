0c7b80c75101a1ed38bee1966c4afc51b032171d
ggilmore <geoffrey@sourcegraph.com>
2019-10-07 15:34:38 -0700

    try parallel yarn

--- enterprise/cmd/frontend/pre-build.sh
+++ enterprise/cmd/frontend/pre-build.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -euxo pipefail
 cd $(dirname "${BASH_SOURCE[0]}")/../..
 
+parallel --help
+
 pushd ..
 echo "--- yarn root"
 yarn --frozen-lockfile --network-timeout 60000
-echo "--- yarn browser"
-(pushd browser && TARGETS=phabricator yarn build && popd)
-echo "--- yarn web"
-(pushd web && NODE_ENV=production yarn -s run build --color && popd)
+(echo "--- yarn browser" && pushd browser && TARGETS=phabricator yarn build && popd) &
+(echo "---yarn web" && pushd web && NODE_ENV=production yarn -s run build --color && popd) &
+wait
 popd
 
 echo "--- generate"
