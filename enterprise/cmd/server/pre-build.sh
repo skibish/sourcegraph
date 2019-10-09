f0477ccc017b6307caa3c7f83c39ac76fea667b6
ggilmore <geoffrey@sourcegraph.com>
2019-10-09 14:43:04 -0700

    update builder

--- enterprise/cmd/server/pre-build.sh
+++ enterprise/cmd/server/pre-build.sh
@@ -3,4 +3,4 @@
 cd $(dirname "${BASH_SOURCE[0]}")/../../..
 set -euxo pipefail
 
-parallel --keep-order --line-buffer --tag --bar {} ::: ./enterprise/cmd/frontend/pre-build.sh ./cmd/management-console/pre-build.sh
+parallel --keep-order --line-buffer --tag {} ::: ./enterprise/cmd/frontend/pre-build.sh ./cmd/management-console/pre-build.sh
